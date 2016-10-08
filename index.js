4be9876f3badae76b026468b1bcb5de383f34bc3
Axel Smeets <murlocbrand@gmail.com>
2016-10-08 16:37:22 +0200

    Config CircleCI and istanbul

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Copyright 2015 Axel Smeets */
+/* Copyright 2015-2016 Axel Smeets */
 function flipshit(err) {
     if (Math.random() > 0.5)
         preflip(err)
@@ -11,8 +11,8 @@ process.on("unhandledRejection", flipshit)
 
 function preflip(err){
     var emote = [
-        '(ﾉ°□°)ﾉ ︵ ┻━┻ ', 
-        '(ﾉ°Д°)ﾉ ︵ ┻━┻ ', 
+        '(ﾉ°□°)ﾉ ︵ ┻━┻ ',
+        '(ﾉ°Д°)ﾉ ︵ ┻━┻ ',
         '(ﾉ◕ヮ◕)ﾉ'
     ][Math.floor(Math.random() * 3)]
     console.error(emote, err.stack)
@@ -24,8 +24,8 @@ function postflip(err){
         'ಠ_ಠ'
     ][Math.floor(Math.random() * 2)]
     var first = err.stack.substring(0, err.stack.indexOf('\n'))
-   
+
     console.error(err.stack.replace(first, first + " " + emote))
 }
 
-module.exports = {}
+module.exports = flipshit
--- test.js
+++ test.js
@@ -1,6 +1,23 @@
-require('./index.js')
+const flipshit = require('./index.js')
 
-if (Math.random() > 0.5)
-    throw Error("I WANNA BE FREEEE")
-else
-    Promise.reject(new SyntaxError("NO PROMISES FOR YOU!"))
+var ctr = 0
+function pass() {
+	ctr = ctr + 1
+	if (ctr === 2) {
+		process.exit(0)
+	}
+}
+
+setTimeout(() => {
+	process.exit(1)
+}, 1000)
+
+process.on("uncaughtException", pass)
+process.on("unhandledRejection", pass)
+
+Promise.reject(new SyntaxError("NO PROMISES FOR YOU!"))
+Promise.reject(new SyntaxError("NO PROMISES FOR YOU!"))
+Promise.reject(new SyntaxError("NO PROMISES FOR YOU!"))
+Promise.reject(new SyntaxError("NO PROMISES FOR YOU!"))
+
+throw Error("I WANNA BE FREEEE")
